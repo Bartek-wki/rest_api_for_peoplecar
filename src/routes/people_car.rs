50e65ec0f1b61cf1a2919c8c3d4ad81807d4b76b
Bartosz Wilki <b.wilki0109@gmail.com>
2022-09-14 10:48:00 +0200

    Change of error handling syntax, removal of redundant returns in functions, change of type from usize to u64

--- src/routes/people_car.rs
+++ src/routes/people_car.rs
@@ -2,31 +2,23 @@ use crate::services;
 
 #[get("/calculateDisselUsageForDistance?<distance>&<year_of_production>&<fuel_usage_per_100_km>")]
 pub fn calculate_dissel_usage_for_distance(
-    distance: usize,
+    distance: u64,
     year_of_production: u16,
     fuel_usage_per_100_km: u8,
 ) -> String {
     let fuel_usage =
         services::people_car::calculate_dissel_usage_for_distance(distance, fuel_usage_per_100_km);
-    return format!("{}", fuel_usage);
+    format!("{}", fuel_usage)
 }
 
 #[get("/probabilityOfUnitInjectorFail?<VIN>")]
 pub fn probability_of_unit_injector_fail(VIN: &str) -> String {
     let probability = services::people_car::probability_of_unit_injector_fail();
 
-    fn check_vin(my_vin: &str) -> Result<&str, &str> {
-        let error = "Wrong VIN";
+    let check_vin = vin::check_validity(VIN);
 
-        if vin::check_validity(my_vin).is_ok() {
-            Ok(my_vin)
-        } else {
-            Err(error)
-        }
-    }
-
-    match check_vin(VIN) {
-        Ok(_v) => format!("{}", probability),
+    match check_vin {
+        Ok(_t) => format!("{}", probability),
         Err(_e) => format!("You have probably entered an incorrect VIN number: {}", VIN),
     }
 }
--- src/services/people_car.rs
+++ src/services/people_car.rs
@@ -1,10 +1,10 @@
 use rand::Rng;
 
-pub fn calculate_dissel_usage_for_distance(distance: usize, fuel_usage_per_100_km: u8) -> f32 {
-    return distance as f32 * fuel_usage_per_100_km as f32 / 100 as f32;
+pub fn calculate_dissel_usage_for_distance(distance: u64, fuel_usage_per_100_km: u8) -> f32 {
+    distance as f32 * fuel_usage_per_100_km as f32 / 100 as f32
 }
 
 pub fn probability_of_unit_injector_fail() -> f32 {
     let random_value = rand::thread_rng().gen_range(0..101);
-    return random_value as f32 / 100 as f32;
+    random_value as f32 / 100 as f32
 }
